203de78756012ebafda4edbee72f63ed61df1074
Maciej Lewicki <lewickimapl@gmail.com>
2021-02-10 22:51:24 +0100

    refactor homescreen ui

--- src/app/screens/HomeScreen.js
+++ src/app/screens/HomeScreen.js
@@ -60,6 +60,25 @@ const Home = ({ navigation }) => {
       credit: true,
     },
   ];
+
+  const Operations = [
+    {
+      key: "1",
+      text: "Send",
+      operation: navigateToSendTokens
+    },
+    {
+      key: "2",
+      text: "Redeem",
+      operation: () => console.log("unimplemented")
+    },
+    {
+      key: "3",
+      text: "Receive",
+      operation: () => console.log("unimplemented")
+    }
+  ];
+
   const { width, height } = Dimensions.get("window");
   const carouselRef = useRef(null);
 
@@ -73,6 +92,7 @@ const Home = ({ navigation }) => {
       </TouchableWithoutFeedback>
     );
   };
+
   const [dragRange, setDragRange] = useState({
     top: height - 80,
     bottom: 160,
@@ -88,218 +108,183 @@ const Home = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <View style={{ paddingTop: 50, paddingHorizontal: 14 }}>
+      <View>
         <View>
-          <View style={{ paddingBottom: "10%" }}>
-            <Text style={{ fontSize: 26, color: "black" }}> Welcome Back,</Text>
-            <Text style={{ fontSize: 26, color: "black", paddingBottom: 10 }}>
-              Gabriel
-            </Text>
-          </View>
-          <View>
-            <Image
-              source={{
-                uri:
-                  "https://scontent.fagc1-2.fna.fbcdn.net/v/t31.0-8/20017494_1570223453010098_2798752839256677297_o.jpg?_nc_cat=108&ccb=2&_nc_sid=730e14&_nc_ohc=Y_mZcslnbsIAX95p6tF&_nc_ht=scontent.fagc1-2.fna&oh=78f39c7c6e0d85e6e503587f409c6d20&oe=603CC6F9",
-              }}
-              style={styles.ProfileImage}
-            />
-            <View style={styles.ProfileImageNotification}></View>
-          </View>
+          <Text style={{ fontSize: 26, color: "black" }}> Welcome back, Gabriel</Text>
+        </View>
+        <View style={styles.profile}>
+          <Image
+            source={{
+              uri:
+                "https://scontent.fagc1-2.fna.fbcdn.net/v/t31.0-8/20017494_1570223453010098_2798752839256677297_o.jpg" +
+                "?_nc_cat=108&ccb=2&_nc_sid=730e14&_nc_ohc=Y_mZcslnbsIAX95p6tF&_nc_ht=scontent.fagc1-2.fna&oh=78f39c7c6e0d85e6e503587f409c6d20&oe=603CC6F9",
+            }}
+            style={styles.ProfileImage}
+          />
+          <Text style={{ color: "black", paddingLeft: 20}}>
+            Account ID
+          </Text>
+          <View style={styles.ProfileImageNotification}></View>
+        </View>
 
-          <View style={{ paddingBottom: "10%" }}>
-            <Text style={{ color: "#ff9100", opacity: 0.6, marginBottom: 20 }}>
-              {" "}
-              Account ID
-            </Text>
-            <View style={{ paddingBottom: "10%" }}>
-              <View style={{ flexDirection: "row" }}>
-                <TouchableOpacity>
-                  <TouchableOpacity style={styles.AddUser} onPress={navigateToSendTokens}>
-                    <View style={styles.AddUserIconbg}>
-                      <MaterialIcons
-                        name="add"
-                        color="white"
-                        size={28}
-                        style={{ alignSelf: "center" }}
-                      />
+        <View>
+          <View style={styles.CardDisplay}>
+            <FlatList
+              contentContainerStyle={styles.CardContentList}
+              horizontal
+              data={Operations}
+              renderItem={({ item }) => {
+                return (
+                  <TouchableOpacity onPress={item.operation}>
+                    <View style={styles.AddUser}>
+                      <View style={styles.AddUserIconbg}>
+                        <MaterialIcons
+                          name="add"
+                          color="white"
+                          size={28}
+                        />
+                      </View>
+                      <Text style={styles.CardText}>
+                        {item.text}
+                      </Text>
                     </View>
-                    <Text style={{ color: "black", paddingBottom: "10%" }}>
-                      {" "}
-                      Send
-                    </Text>
                   </TouchableOpacity>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.AddUser}>
-                  <View style={styles.AddUserIconbg}>
-                    <MaterialIcons
-                      name="add"
-                      color="white"
-                      size={28}
-                      style={{ alignSelf: "center" }}
-                    />
-                  </View>
-                  <Text style={{ color: "black", paddingBottom: "10%" }}>
-                    {" "}
-                    Redeem
-                  </Text>
-                </TouchableOpacity>
+                );
+              }}
+            />
 
-                <TouchableOpacity style={{ borderColor: "white" }}>
-                  <View style={styles.AddUser}>
-                    <View style={styles.AddUserIconbg}>
-                      <MaterialIcons
-                        name="add"
-                        color="white"
-                        size={28}
-                        style={{ alignSelf: "center" }}
-                      />
-                    </View>
-                    <Text style={{ color: "black", paddingBottom: "10%" }}>
-                      {" "}
-                      Recieve{" "}
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-              </View>
-
-              <FlatList
-                style={{ paddingTop: "10%" }}
-                inverted
-                horizontal
-                data={Users}
-                renderItem={({ item }) => {
-                  return (
+            <FlatList
+              contentContainerStyle={styles.CardContentList}
+              horizontal
+              data={Users}
+              renderItem={({ item }) => {
+                return (
+                  <TouchableOpacity>
                     <View style={styles.AddUser}>
                       <Image
                         style={styles.AddUserIconbg}
                         source={{ uri: item.userImage }}
                       />
-                      <Text
-                        style={{ color: "black", justifyContent: "center" }}
-                      >
+                      <Text style={styles.CardText} >
                         {item.userName}
                       </Text>
                     </View>
-                  );
-                }}
-              />
-            </View>
-          </View>
-        </View>
-        <View style={{ paddingTop: 110 }}>
-          <View style={{ flex: 1 }}>
-            <SlidingUpPanel
-              ref={ModalRef}
-              draggableRange={dragRange}
-              animatedValue={_draggedValue}
-              backdropOpacity={0}
-              snappingPoints={[360]}
-              height={height + 20}
-              friction={0.9}
-            >
-              <View
-                style={{
-                  flex: 1,
-                  backgroundColor: "white",
-                  borderColor: "black",
-                  borderRadius: 24,
-                  paddingTop: 10,
-                }}
-              >
-                <View style={styles.PanelHandle}></View>
-                <View>
-                  <Text style={{ marginVertical: 16, color: "black" }}>
-                    {" "}
-                    Recent Transactions
-                  </Text>
-                </View>
-                <View style={{ height: 500, paddingBottom: 10 }}>
-                  <FlatList
-                    data={Users}
-                    keyExtractor={(item) => item.key}
-                    renderItem={({ item }) => {
-                      return (
-                        <View style={styles.PanelItemContainer}>
-                          <View
-                            style={{
-                              flexDirection: "row",
-                              alignItems: "center",
-                            }}
-                          >
-                            <View style={{ marginRight: 10 }}>
-                              <Image
-                                source={{ uri: item.userImage }}
-                                style={styles.PanelImage}
-                              />
-                            </View>
-                            <View>
-                              <Text style={{ fontSize: 14, color: "black" }}>
-                                {item.userName}
-                              </Text>
-                              <Text
-                                style={{
-                                  fontSize: 10,
-                                  color: "black",
-                                  opacity: 0.6,
-                                }}
-                              >
-                                {item.transactionDate}
-                              </Text>
-                            </View>
-                          </View>
-                          <View
-                            style={{
-                              flexDirection: "row",
-                              alignItems: "center",
-                            }}
-                          >
-                            <Text
-                              style={{
-                                fontSize: 16,
-                                color: "black",
-                                marginHorizontal: 2,
-                              }}
-                            >
-                              {item.amount}{" "}
-                            </Text>
-
-                            {item.credit ? (
-                              <MaterialIcons
-                                name="arrow-drop-up"
-                                size={22}
-                                color="green"
-                              />
-                            ) : (
-                              <MaterialIcons
-                                name="arrow-drop-down"
-                                size={22}
-                                color="#ff3838"
-                              />
-                            )}
-                          </View>
-                        </View>
-                      );
-                    }}
-
-                    //view history
-                  />
-                </View>
-                <View
-                  style={{ flexDirection: "row", justifyContent: "flex-end" }}
-                >
-                  <TouchableOpacity style={styles.PanelButton}>
-                    <Text style={styles.PanelButtonText}>
-                      View Full History
-                    </Text>
                   </TouchableOpacity>
-                </View>
-              </View>
-            </SlidingUpPanel>
+                );
+              }}
+            />
           </View>
         </View>
       </View>
+
+      <View>
+        <SlidingUpPanel
+          ref={ModalRef}
+          draggableRange={dragRange}
+          animatedValue={_draggedValue}
+          backdropOpacity={0}
+          snappingPoints={[360]}
+          height={height + 20}
+          friction={0.9}
+        >
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "white",
+              borderColor: "black",
+              borderRadius: 6,
+            }}
+          >
+            <View style={styles.PanelHandle}></View>
+            <View>
+              <Text style={{ marginVertical: 16, color: "black" }}>
+                Recent Transactions
+              </Text>
+            </View>
+            <View style={{ height: 500, paddingBottom: 10 }}>
+              <FlatList
+                data={Users}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) => {
+                  return (
+                    <View style={styles.PanelItemContainer}>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          alignItems: "center",
+                        }}
+                      >
+                        <View style={{ marginRight: 10 }}>
+                          <Image
+                            source={{ uri: item.userImage }}
+                            style={styles.PanelImage}
+                          />
+                        </View>
+                        <View>
+                          <Text style={{ fontSize: 14, color: "black" }}>
+                            {item.userName}
+                          </Text>
+                          <Text
+                            style={{
+                              fontSize: 10,
+                              color: "black",
+                              opacity: 0.6,
+                            }}
+                          >
+                            {item.transactionDate}
+                          </Text>
+                        </View>
+                      </View>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Text
+                          style={{
+                            fontSize: 16,
+                            color: "black",
+                            marginHorizontal: 2,
+                          }}
+                        >
+                          {item.amount}{" "}
+                        </Text>
+
+                        {item.credit ? (
+                          <MaterialIcons
+                            name="arrow-drop-up"
+                            size={22}
+                            color="green"
+                          />
+                        ) : (
+                          <MaterialIcons
+                            name="arrow-drop-down"
+                            size={22}
+                            color="#ff3838"
+                          />
+                        )}
+                      </View>
+                    </View>
+                  );
+                }}
+
+                //view history
+              />
+            </View>
+            <View
+              style={{ flexDirection: "row", justifyContent: "flex-end" }}
+            >
+              <TouchableOpacity style={styles.PanelButton}>
+                <Text style={styles.PanelButtonText}>
+                  View Full History
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </SlidingUpPanel>
     </View>
+  </View>
   );
 };
 
@@ -307,7 +292,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    paddingTop: 0,
+
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+
+    paddingTop: 30,
+    paddingLeft: 10,
+    paddingBottom: 10,
+    paddingRight: 10
+  },
+
+  profile: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    padding: 10
   },
 
   ProfileImage: {
@@ -315,6 +316,7 @@ const styles = StyleSheet.create({
     height: 80,
     borderRadius: 40,
   },
+
   ProfileImageNotification: {
     height: 12,
     width: 12,
@@ -325,26 +327,49 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "#000",
   },
+
+  CardDisplay: {
+    paddingTop: 25,
+    paddingLeft: 15,
+    paddingBottom: 50,
+    paddingRight: 15
+  },
+
+  CardContentList: { 
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    overflow: "scroll"
+  },
+
+  CardText: {
+    color: "black",
+    textAlign: "center" 
+  },
+
   AddUser: {
     height: 140,
     width: 100,
     borderColor: "black",
     borderWidth: 1,
-    justifyContent: "center",
+    justifyContent: "space-around",
     alignItems: "center",
     backgroundColor: "white",
     borderRadius: 10,
-    marginRight: 14,
+    margin: 7,
   },
+
   AddUserIconbg: {
     width: 70,
     height: 70,
     backgroundColor: "orange",
     borderColor: "orange",
     borderRadius: 10,
-    marginBottom: 10,
+    margin: 10,
     justifyContent: "center",
+    alignItems: "center"
   },
+
   PanelHandle: {
     height: 6,
     width: 50,
@@ -353,6 +378,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     marginTop: 6,
   },
+
   PanelItemContainer: {
     borderWidth: 0.4,
     borderColor: "#666",
@@ -363,12 +389,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 20,
   },
+
   PanelImage: {
     width: 30,
     height: 30,
     backgroundColor: "#000",
     borderRadius: 40,
   },
+
   PanelButton: {
     padding: 14,
     width: 200,
@@ -376,6 +404,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#1c1c1c",
     borderRadius: 10,
   },
+
   PanelButtonText: {
     fontSize: 16,
     color: "#fff",
